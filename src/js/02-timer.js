3e7488652a2dc318d80b6fc31addc640a829f533
Olena Stasiuk <estasjuk@gmail.com>
2022-11-22 01:24:05 +0200

    add task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,13 +1,19 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+let remainTime = 0;
+let currentTime = 0;
+let selectedTime = 0;
+
 const options = {
   enableTime: true,        //включает выбор времени
   time_24hr: true,         //Displays time picker in 24 hour mode without AM/PM selection when enabled.
   defaultDate: new Date(), //Sets the initial selected date(s)
   minuteIncrement: 1,      //Adjusts the step for the minute input (incl. scrolling)
   onClose(selectedDates) {
-    if ((Date.now() - selectedDates[0]) > 0) {
+    selectedTime = selectedDates[0].getTime();
+    currentTime = Date.now();
+    if ((currentTime - selectedTime) > 0) {
       refs.startBtn.setAttribute('disabled', true);
       return alert("Please choose a date in the future");
     }
@@ -15,11 +21,13 @@ const options = {
       refs.startBtn.removeAttribute('disabled');
     }
     console.log(selectedDates[0]);
+    
   },
-  
 };
 
 flatpickr("#datetime-picker", options);
+
+
 
 const refs = {
   startBtn: document.querySelector("button[data-start]"),
@@ -33,9 +41,30 @@ refs.startBtn.setAttribute('disabled', true);
 
 
 function onCountdown(e) { 
-  console.log(e.currentTarget);
-}
+  setInterval(() => {
+    remainTime = selectedTime - currentTime;
+    console.log(remainTime)
+  }, 1000);
+};
 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+};
 
 refs.startBtn.addEventListener("click", onCountdown);
 
